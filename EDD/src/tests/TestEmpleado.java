bb2bd0673c2a89b3b31afb063351a0e973d2da4e
alejandro-999 <elalejandrito99@gmail.com>
2020-05-12 14:26:44 +0200

    Actividad JUnit

--- EDD/src/tests/TestEmpleado.java
+++ EDD/src/tests/TestEmpleado.java
@@ -24,4 +24,88 @@ public class TestEmpleado
         assertEquals(4,e.getNumeroEmpleado());
 	}
 
+	@Test 
+	public void estableceNombreEmpleado() 
+	{
+		Empleado e = new Empleado();
+        try 
+        {
+            e.estableceNombreEmpleado("AlejandroGonzalez");
+        } catch (NombreEmpleadoException ex) 
+        {
+        	fail();
+        }
+        assertEquals("AlejandroGonzalez",e.getNombreEmpleado());
+	}
+	
+	@Test 
+	public void  estableceMesesTrabajo() 
+	{
+		Empleado e = new Empleado();
+        try 
+        {
+            e.estableceMesesTrabajo("7");
+        } catch (MesesTrabajoException ex) 
+        {
+        	fail();
+        }
+        assertEquals(7,e.getMesesTrabajo());
+	}
+	
+	@Test 
+	public void establecerSerDirectivo() 
+	{
+		Empleado e = new Empleado();
+        try 
+        {
+            e.establecerSerDirectivo("+");
+        } catch (CargoException ex) 
+        {
+        	fail();
+        }
+        assertEquals(true,e.getDirectivo());
+	}
+	
+	@Test 
+	public void calcularPrima() 
+	{
+		Empleado e = new Empleado();
+		    try 
+	        {
+	            e.establecerNumeroEmpleado("4");
+	        } catch (NumeroEmpleadoException ex) 
+	        {
+	        	fail();
+	        }
+	        assertEquals(4,e.getNumeroEmpleado());
+	        try 
+	        {
+	            e.estableceNombreEmpleado("AlejandroGonzalez");
+	        } catch (NombreEmpleadoException ex) 
+	        {
+	        	fail();
+	        }
+	        assertEquals("AlejandroGonzalez",e.getNombreEmpleado());
+	        try 
+	        {
+	            e.estableceMesesTrabajo("7");
+	        } catch (MesesTrabajoException ex) 
+	        {
+	        	fail();
+	        }
+	        assertEquals(7,e.getMesesTrabajo());
+	        try 
+	        {
+	            e.establecerSerDirectivo("+");
+	        } catch (CargoException ex) 
+	        {
+	        	fail();
+	        }
+	        assertEquals(true,e.getDirectivo());
+	        
+	}
+	
+	
+	
+
 }
